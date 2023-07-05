e7e5128b71420e630ea1b33e1e16b328168468f8
Carlos Maniero <carlos@maniero.me>
2023-07-05 13:16:57 -0300

    keybindings: register bash like key binding for cmode
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

--- keybindings.lua
+++ keybindings.lua
@@ -7,8 +7,18 @@ function register_buffer_keybindings()
   vim.keymap.set('n', 'bc', require('misc.buffers').buffer_choose)
 end
 
+function register_bash_like_navigation_on_command_prompt()
+  vim.api.nvim_set_keymap('c', '<C-A>', '<Home>',  {noremap = true})
+  vim.api.nvim_set_keymap('c', '<C-E>', '<End>',   {noremap = true})
+  vim.api.nvim_set_keymap('c', '<C-F>', '<Right>', {noremap = true})
+  vim.api.nvim_set_keymap('c', '<C-B>', '<Left>',  {noremap = true})
+  vim.api.nvim_set_keymap('c', '<C-N>', '<Down>',  {noremap = true})
+  vim.api.nvim_set_keymap('c', '<C-P>', '<Up>',    {noremap = true})
+end
+
 function keybindings.register()
   register_buffer_keybindings()
+  register_bash_like_navigation_on_command_prompt()
 end
 
 return keybindings
