245cfd9a4de67a0f6a7573e1a23cf7ddc4e16d71
Carlos Maniero <carlos@maniero.me>
2023-07-11 03:30:19 -0300

    paredit: implements the find close
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

--- paredit.lua
+++ paredit.lua
@@ -5,7 +5,8 @@ local function is_token_a_string(line, column)
   return vim.fn.synIDattr(synId, 'name') == 'String' or vim.fn.synIDattr(vim.fn.synIDtrans(synId), 'name') == 'String'
 end
 
-function OpenParenLocation()
+function OpenParenLocation(parens)
+  parens = parens or { open = '(', close = ')' }
   local current_buffer = 0
   local current_window = 0
 
@@ -37,7 +38,7 @@ function OpenParenLocation()
     for column_index = line_len, 1, -1 do
       local current_character = line:sub(column_index, column_index)
 
-      if current_character == '(' then
+      if current_character == parens.open then
         local line_number = line_index_to_line_number(line_index)
 
         if not is_token_a_string(line_number, column_index) then
@@ -51,11 +52,79 @@ function OpenParenLocation()
         end
       end
 
-      if current_character == ')' then
+      if current_character == parens.close then
         local line_number = line_index_to_line_number(line_index)
 
         if not is_token_a_string(line_number, column_index) then
-          close_paren_count = close_paren_count + 1
+          -- That's ok if the current character is a close paren
+          if line_index ~= in_range_lines_len or column_index ~= line_len then
+            close_paren_count = close_paren_count + 1
+          end
+        end
+      end
+    end
+  end
+  return nil
+end
+
+function CloseParenLocation(parens)
+  parens = parens or { open = '(', close = ')' }
+
+  local current_buffer = 0
+  local current_window = 0
+
+  local cursor_line = vim.fn.line('.')
+  local cursor_column = vim.fn.col('.')
+
+  local window_height = vim.api.nvim_win_get_height(current_window)
+  local total_of_lines = vim.api.nvim_buf_line_count(current_buffer)
+
+  local range_start = cursor_line - 1
+  local range_end = math.min(window_height + cursor_line, total_of_lines)
+
+  local in_range_lines = vim.api.nvim_buf_get_lines(current_buffer, range_start, range_end, true)
+  local in_range_lines_len = #in_range_lines
+
+  local open_paren_count = 1
+
+  local line_index_to_line_number = function(line_index)
+    return cursor_line + line_index - 1
+  end
+
+  for line_index = 1, in_range_lines_len do
+    local line = in_range_lines[line_index]
+    local start_from = 1
+    local line_len = string.len(line)
+
+    if line_index == 1 then
+      start_from = cursor_column
+    end
+
+    for column_index = start_from, line_len  do
+      local current_character = line:sub(column_index, column_index)
+
+      if current_character == parens.close then
+        local line_number = line_index_to_line_number(line_index)
+
+        if not is_token_a_string(line_number, column_index) then
+          open_paren_count = open_paren_count - 1
+          if open_paren_count == 0 then
+            return {
+              line = line_number,
+              column = column_index
+            }
+          end
+        end
+      end
+
+      if current_character == parens.open then
+        local line_number = line_index_to_line_number(line_index)
+
+        if not is_token_a_string(line_number, column_index) then
+          -- That's ok if the current character is a open paren
+          if line_index > 1 or column_index ~= start_from then
+            open_paren_count = open_paren_count + 1
+          end
         end
       end
     end
@@ -66,10 +135,25 @@ end
 function HI_open_paren()
   vim.api.nvim_buf_clear_namespace(0, hi_namespace, 0, -1)
 
-  local open_paren = OpenParenLocation()
+  local parens = {
+    { open = '(', close = ')' },
+    { open = '[', close = ']' },
+    { open = '{', close = '}' }
+  }
 
-  if open_paren then
-    vim.api.nvim_buf_add_highlight(0, hi_namespace, 'MatchParen', open_paren.line - 1, open_paren.column - 1, open_paren.column)
+  for _, paren in ipairs(parens) do
+    local open_paren = OpenParenLocation(paren)
+
+    if open_paren then
+      vim.api.nvim_buf_add_highlight(0, hi_namespace, 'MatchParen', open_paren.line - 1, open_paren.column - 1, open_paren.column)
+
+      local close_paren = CloseParenLocation(paren)
+
+      if close_paren then
+        vim.api.nvim_buf_add_highlight(0, hi_namespace, 'MatchParen', close_paren.line - 1, close_paren.column - 1, close_paren.column)
+      end
+      return
+    end
   end
 end
 
